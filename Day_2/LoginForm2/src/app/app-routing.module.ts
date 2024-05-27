899997576f4fff1b5759f691a750f6506cc2a5f4
raagi01 <126334141+raagi01@users.noreply.github.com>
2024-05-27 16:27:27 +0530

    Day_2

---
+++ Day_2/LoginForm2/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Day_2/LoginForm2/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'LoginForm2';
+  email = " ";
+  password = " ";
+
+  constructor(private router: Router) {}
+
+  onClick() {
+    this.router.navigate(['/home']);
+  }
+}
