70fda3de471c4108cd12715c62063640d083e6a7
raagi01 <126334141+raagi01@users.noreply.github.com>
2024-06-05 14:01:30 +0530

    Day_5

---
+++ Day_5/Books/DataAccessLayer/DALStatics.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DALStatics
+    {
+        public static List<UserInfo> Users = new List<UserInfo>()
+        {
+            new UserInfo()
+            {
+                Username = "raagi_admin",
+                EmailAddress = "raagi@bvm",
+                Password = "Admin_pwd123",
+                GivenName = "Raagi",
+                Surname = "Raj",
+                Role = "Administrator"
+            },
+
+            new UserInfo()
+            {
+                Username = "john_reader",
+                EmailAddress = "john.user@tatvasoft.com",
+                Password = "john_pwd",
+                GivenName = "John",
+                Surname = "Doe",
+                Role = "BookUser"
+            },
+        };
+    }
+}
